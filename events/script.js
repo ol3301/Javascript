a3d52f791eaa31f984d2e5f373b73ffe3699da74
mazda <ol.kyzy@gmail.com>
2019-03-24 12:44:22 +0200

    доделал светофор

--- events/script.js
+++ events/script.js
@@ -1,3 +1,16 @@
 function next(){
-    fig1.style="background-color:red;";
+    if(fig1.style['background-color'] == "red"){
+        fig1.style['background-color']="gray"
+        fig2.style['background-color']="yellow";
+    }
+    else if(fig2.style['background-color']=="yellow"){
+        fig2.style['background-color']="gray";
+        fig3.style['background-color']="green";
+    }
+    else if(fig3.style['background-color']=="green"){
+        fig3.style['background-color']="gray"
+    }
+    else{
+        fig1.style['background-color']="red"
+    }
 }
